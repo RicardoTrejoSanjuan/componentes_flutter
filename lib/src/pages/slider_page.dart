b6a16a4cccc9f20b631c1bea80b0059bc28799f2
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-01-19 12:26:22 -0600

    Widget - CheckBox basico

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -8,6 +8,7 @@ class SliderPage extends StatefulWidget {
 class _SliderPageState extends State<SliderPage> {
 
   double _valorSlide = 100.0;
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _crearSlider(),
+            _checkBox(),
             Expanded(
               child: _crearImagen()
             ),
@@ -37,7 +39,7 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlide,
       min: 10.0,
       max: 400.0,
-      onChanged: (valor) {
+      onChanged: (_bloquearCheck) ? null : (valor) {
         setState(() {
           _valorSlide = valor;
         });
@@ -45,6 +47,17 @@ class _SliderPageState extends State<SliderPage> {
     );
   }
 
+  Widget _checkBox() {
+    return Checkbox(
+      value: _bloquearCheck,
+      onChanged: (valor) {
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+    );
+  }
+  
   Widget _crearImagen() {
     return Image(
       image: NetworkImage('https://pngimage.net/wp-content/uploads/2018/06/portgas-d-ace-png-3.png'),
