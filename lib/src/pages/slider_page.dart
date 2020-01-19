3c4ebb4526998dc9bdffb74d60dbf720023caa2f
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-01-19 12:21:07 -0600

    Widget - Slider

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -6,12 +6,50 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
+
+  double _valorSlide = 100.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Slider Appp'),
       ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(
+              child: _crearImagen()
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigo,
+      label: 'Tamaño de la imagen',
+      // divisions: 20,
+      value: _valorSlide,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor) {
+        setState(() {
+          _valorSlide = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image: NetworkImage('https://pngimage.net/wp-content/uploads/2018/06/portgas-d-ace-png-3.png'),
+      width: _valorSlide,
+      fit: BoxFit.contain,
     );
   }
 }
