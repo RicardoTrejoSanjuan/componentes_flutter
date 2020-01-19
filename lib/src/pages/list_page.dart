569a0925f402d4c64a3421404af50c4260698f8c
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-01-19 13:10:27 -0600

    InfiniteScroll

--- lib/src/pages/list_page.dart
+++ lib/src/pages/list_page.dart
@@ -6,12 +6,57 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = new List();
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _agregar10();
+
+    _scrollController.addListener(() {
+      print('SCROLL!!');
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Listas Page'),
       ),
+      body: _crearLista(),
     );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        final imagen = _listaNumeros[index];
+        return FadeInImage(
+          image: NetworkImage('https://i.picsum.photos/id/$imagen/800/600.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
+    );
+  }
+
+  void _agregar10() {
+    for (var i = 0; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {});
   }
 }
