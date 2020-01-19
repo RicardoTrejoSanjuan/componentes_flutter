e5726a8f175fa4fe751172e5d0a2dea642586ab2
Ricardo Trejo Sanjuan <ricardo.trejo@stratplus.net>
2020-01-19 08:31:28 -0600

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(4.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/one-piece-ace-spade-aditya-sena.jpg'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.orange,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://a-static.besthdwallpaper.com/one-piece-portgas-d-ace-piratas-de-barbablanca-papel-pintado-16187_L.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(
+            milliseconds: 200
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back_ios),
